b847820bba2b86f8496c47043a532611ddd66a89
AaronDemond <demondsoftware@gmail.com>
2026-08-20 17:13:20 -0300

    Initial base css created, index page started

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    def test_index_renders_base_template_with_navigation(self):
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertContains(response, 'C. Aaron Demond')
+        self.assertContains(response, 'Home')
+        self.assertContains(response, 'Privacy')
+        self.assertNotContains(response, '2025 Alex Morgan')
