d7711dad475e33792faafc39221b7ea44b92db8f
AaronDemond <demondsoftware@gmail.com>
2026-08-21 05:10:31 -0300

    moved projects code to db

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Project,
+    ProjectKeyArchitectureDecision,
+    ProjectResult,
+    ProjectScreenshot,
+    ProjectTool,
+)
+
+
+class ProjectScreenshotInline(admin.TabularInline):
+    model = ProjectScreenshot
+    extra = 1
+
+
+class ProjectToolInline(admin.TabularInline):
+    model = ProjectTool
+    extra = 1
+
+
+class ProjectResultInline(admin.TabularInline):
+    model = ProjectResult
+    extra = 1
+
+
+class ProjectKeyArchitectureDecisionInline(admin.TabularInline):
+    model = ProjectKeyArchitectureDecision
+    extra = 1
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'git_link')
+    search_fields = ('name', 'description', 'git_link')
+    inlines = (
+        ProjectScreenshotInline,
+        ProjectToolInline,
+        ProjectResultInline,
+        ProjectKeyArchitectureDecisionInline,
+    )
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    architecture = models.FileField(upload_to='project_architectures/', blank=True)
+    git_link = models.CharField(max_length=500, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectScreenshot(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='screenshots',
+    )
+    file = models.FileField(upload_to='project_screenshots/')
+
+    def __str__(self):
+        return f'{self.project.name} screenshot'
+
+
+class ProjectTool(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='tools',
+    )
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class ProjectResult(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='results',
+    )
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class ProjectKeyArchitectureDecision(models.Model):
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='key_architecture_decisions',
+    )
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
